205c2495912977336b7c42109e54716a00192672
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-06-16 13:54:57 -0400

    calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,3 +9,13 @@ end
 def sum(b)
 	b.inject(0) { |sum,a| sum += a }
 end
+
+def multiply(c)
+	final = 1.0
+	c.each { |i| final *= 1}
+	final
+end
+
+def factorial(b)
+	b.inject(0) { |product,a| product *= a }
+end
